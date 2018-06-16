99ea13e702152b4a77b51688aaabaea2b68886da
Muhua Jiang <jmh_nt@qq.com>
2018-06-16 18:57:41 +0800

    'init

---
+++ static/js/0.c38ced6a944bc9b237e7.js
@@ -0,0 +1,2 @@
+webpackJsonp([0,1],{305:function(e,t,n){var s=n(6)(null,n(312),null,null);e.exports=s.exports},312:function(e,t){e.exports={render:function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("section",{staticClass:"test-page"},[e._v("\n  test page\n")])},staticRenderFns:[]}}});
+//# sourceMappingURL=0.c38ced6a944bc9b237e7.js.map
